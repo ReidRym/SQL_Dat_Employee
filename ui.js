71a3cb17e473150e468e1f53528d6f09b916339c
Ryan R <irishreid55@gmail.com>
2023-05-10 14:27:49 -0700

    updating ui.js file

--- ui.js
+++ ui.js
@@ -100,7 +100,120 @@ function viewRoles() {
               );
             });
           }
+          
+          function addRole() {
+            connection.query("SELECT * FROM department", function (err, res) {
+              if (err) throw err;
+              //asking for the three properties on the roles table      
+              inquirer.prompt([
+                {
+                  name: "title",
+                  type: "input",
+                  message: "What is the title of the new role?"
+                },
+                {
+                  name: "salary",
+                  type: "input",
+                  message: "What is the salary of this position?",
+                },
+                {
+                  name: "deptId",
+                  type: "list",
+                  message: "Select a department for this role",
+                  choices: res.map(item => item.name)
+                }
+              ]).then(function (answers) {
+                const selectedDept = res.find(dept => dept.name === answers.deptId);
+                connection.query("INSERT INTO roles SET ?",
+                  {
+                    title: answers.title,
+                    salary: answers.salary,
+                    dept_id: selectedDept.id
+                  },
+                  function (err, res) {
+                    if (err) throw err;
+                    console.log("New role added!\n");
+                    initPrompt();
+                  }
+                );
+              });
+            })
+          };
 
+          function addEmployee() {
+            connection.query("SELECT * FROM roles", function (err, res) {
+              if (err) throw err;
+              //asking for the three properties on the roles table      
+              inquirer.prompt([
+                {
+                  name: "firstName",
+                  type: "input",
+                  message: "What is the employee's first name?"
+                },
+                {
+                  name: "lastName",
+                  type: "input",
+                  message: "What is the employee's last name?",
+                },
+                {
+                  name: "roleId",
+                  type: "list",
+                  message: "Select a role for this employee",
+                  choices: res.map(item => item.title)
+                }
+              ]).then(function (answers) {
+                const selectedRole = res.find(role => role.title === answers.roleId);
+                connection.query("INSERT INTO employee SET ?",
+                  {
+                    first_name: answers.firstName,
+                    last_name: answers.lastName,
+                    role_id: selectedRole.id
+                  },
+                  function (err, res) {
+                    if (err) throw err;
+                    console.log("New employee added!\n");
+                    initPrompt();
+                  }
+                );
+              });
+            })
+          };
+
+          function updateEmp() {
+            connection.query("SELECT * FROM employee", function (err, res) {
+              if (err) throw err;
+              //asking for the three properties on the roles table      
+              inquirer.prompt([
+                {
+                  name: "employee",
+                  type: "list",
+                  message: "Which employee would you like to update?",
+                  choices: res.map(item => item.first_name)
+                },
+                {
+                  name: "newRole",
+                  type: "list",
+                  message: "Select a new role for this employee",
+                  choices: res.map(item => item.title)
+                }
+              ]).then(function (answers) {
+                const selectedRole = res.find(role => role.title === answers.newRole);
+                connection.query("UPDATE employee SET ? WHERE ?",
+                  {
+                    role_id: selectedRole.id
+                  },
+                  {
+                    first_name: answers.employee
+                  },
+                  function (err, res) {
+                    if (err) throw err;
+                    console.log("Employee updated!\n");
+                    initPrompt();
+                  }
+                );
+              });
+            })
+          }
 
 
 
